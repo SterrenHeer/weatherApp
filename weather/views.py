c13e5a4ee045d88c922c6833d4fcd32eb0741e83
SterrenHeer <sterrenheer@gmail.com>
2022-01-03 14:07:05 +0300

    Add weather display

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
 
-# Create your views here.
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=bf67f45c5820583170fa862576056717'
+    cities = City.objects.all()
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+    form = CityForm()
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        print(r)
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'weather/weather.html', context)
